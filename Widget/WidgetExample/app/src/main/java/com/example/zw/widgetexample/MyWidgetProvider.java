2e06543795bcfc98bc5cac31296b34cf51e83697
zw <1622993630@qq.com>
2019-06-07 23:53:37 +0800

    update Widget example

--- Widget/WidgetExample/app/src/main/java/com/example/zw/widgetexample/MyWidgetProvider.java
+++ Widget/WidgetExample/app/src/main/java/com/example/zw/widgetexample/MyWidgetProvider.java
@@ -3,39 +3,49 @@ package com.example.zw.widgetexample;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.Toast;
 
 public class MyWidgetProvider extends AppWidgetProvider {
     public static final String WIDGET_ACTION="com.example.zw.widgetexample.widget.action";
+    private  ComponentName mComponentName;
+    private  RemoteViews mRemoteViews;
     //widget被更新时调用
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
-
-        RemoteViews remoteViews=new RemoteViews(context.getPackageName(),R.layout.widget_layout);
-        remoteViews.setTextViewText(R.id.mTextView,"change text");
-        Intent intent=new Intent();
+        mRemoteViews=new RemoteViews(context.getPackageName(),R.layout.widget_layout);
+        mRemoteViews.setTextViewText(R.id.mTextView,"change text");
+        Intent intent=new Intent(context,MainActivity.class);
         intent.setAction(WIDGET_ACTION);
-        PendingIntent pendingIntent=PendingIntent.getBroadcast(context,0,intent,0);
-        remoteViews.setOnClickPendingIntent(R.id.mBtn,pendingIntent);
-        appWidgetManager.updateAppWidget(appWidgetIds,remoteViews);
-        for (int appWidgetId:appWidgetIds){
-            appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
-        }
+        //PendingIntent pendingIntent=PendingIntent.getBroadcast(context,0,intent,0);
+        PendingIntent pendingIntent=PendingIntent.getActivity(context,200,intent,PendingIntent.FLAG_CANCEL_CURRENT);
+        mRemoteViews.setOnClickPendingIntent(R.id.mBtn,pendingIntent);
+        mComponentName=new ComponentName(context,MyWidgetProvider.class);
+        appWidgetManager.updateAppWidget(mComponentName,mRemoteViews);
+//        appWidgetManager.updateAppWidget(appWidgetIds,remoteViews);
+//        for (int appWidgetId:appWidgetIds){
+//            appWidgetManager.updateAppWidget(appWidgetId,remoteViews);
+//        }
     }
     //接收widget点击时发送的广播
     @Override
     public void onReceive(Context context, Intent intent) {
-        //super.onReceive(context, intent);
+        super.onReceive(context, intent);
         String action=intent.getAction();
-        //if(action.equals(WIDGET_ACTION)){
+        Log.d("zw_debug","here");
+        if(action.equals(WIDGET_ACTION)){
             Toast.makeText(context,"widget click",Toast.LENGTH_SHORT).show();
-        //}
+        }
+        AppWidgetManager.getInstance(context).updateAppWidget(mComponentName,mRemoteViews);
     }
+
+    
     //widget删除时调用
     @Override
     public void onDeleted(Context context, int[] appWidgetIds) {
