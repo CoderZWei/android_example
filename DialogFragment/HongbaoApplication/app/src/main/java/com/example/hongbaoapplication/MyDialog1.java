ca8150cc1481667de9efd0df2bf88f82b63b1f33
zw <1622993630@qq.com>
2019-06-14 14:32:53 +0800

    updata DialogFragment

---
+++ DialogFragment/HongbaoApplication/app/src/main/java/com/example/hongbaoapplication/MyDialog1.java
@@ -0,0 +1,118 @@
+package com.example.hongbaoapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.security.acl.LastOwnerException;
+
+public class MyDialog extends DialogFragment implements View.OnTouchListener {
+    private ImageView mImageView;
+    private Button mBtn;
+    private static float mScale;
+    private int mImageViewHeight;
+    private Context mContext;
+    private String mDialogTheme="1";
+    private View mView;
+    public MyDialog(Context context) {
+        this.mContext=context;
+    }
+
+    public MyDialog() {
+    }
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(android.app.DialogFragment.STYLE_NO_FRAME, R.style.MyDialogStyle);
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        mView=inflater.inflate(R.layout.dialog_layout,container);
+        mImageView=(ImageView)mView.findViewById(R.id.imgView);
+        mView.setOnTouchListener(this);
+        switch (mDialogTheme){
+            case "1":
+                mImageView.setBackground(mContext.getResources().getDrawable(R.drawable.img1));
+                break;
+            case "2":
+                mImageView.setBackground(mContext.getResources().getDrawable(R.drawable.img2));
+                break;
+            case "3":
+                mImageView.setBackground(mContext.getResources().getDrawable(R.drawable.img3));
+                break;
+        }
+        mImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                Log.d("zw_debug","imageview touch");
+                switch (event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        float y=event.getY();
+                        if(pix2dip(y)>mImageViewHeight/2){
+                            //下半部分可点击
+                            dismiss();
+
+                            //HSCashCenterManager.getInstance().startLotteryWheel(this.mContext);
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+        ConstraintLayout.LayoutParams params=(ConstraintLayout.LayoutParams) mImageView.getLayoutParams();
+        mImageViewHeight=pix2dip(params.height);
+        mBtn=(Button)mView.findViewById(R.id.close_btn);
+        //mBtn.getLayoutParams().height=pix2dip(120);
+        //mBtn.getLayoutParams().width=pix2dip(120);
+        ConstraintLayout.LayoutParams layoutParams=(ConstraintLayout.LayoutParams) mBtn.getLayoutParams();
+        layoutParams.height=pix2dip(120);
+        layoutParams.width=pix2dip(120);
+        layoutParams.rightMargin=pix2dip(96);
+        layoutParams.topMargin=pix2dip(162);
+
+        mBtn.setLayoutParams(layoutParams);
+
+        mBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        return mView;
+    }
+    private int pix2dip(float pixValue){
+        return (int)(pixValue/mScale+0.5f);
+    }
+    public void setScale(float scale){
+        this.mScale=scale;
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+            Log.d("zw_debug","parent_touch");
+            dismiss();
+        return false;
+    }
+
+    public void setDialogTheme(String type){
+        this.mDialogTheme=type;
+    }
+
+}
